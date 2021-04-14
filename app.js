7f0e3e2c8ca33353040a80c05640d47169166c1a
karakotineeraj <neerajkarakoti13@gmail.com>
2021-04-14 17:46:52 +0530

    Added sequelize

--- app.js
+++ app.js
@@ -4,10 +4,41 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const pool = require("./db");
+const { Sequelize, DataTypes, Model } = require("sequelize");
 
 // req.body
 app.use(express.json());
+
+// Setting up the DB connection
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+    acquire: 10000
+  }
+});
+
+const Todo = sequelize.define("todo", {
+  todoId: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.TEXT,
+    required: true,
+    unique: true,
+    allowNull: false
+  }
+});
+
+sequelize.sync()
+.catch(function(err) {
+  console.log(err);
+});
 
 /*
   ROUTES
@@ -15,66 +46,72 @@ app.use(express.json());
 
 // Get all todos
 app.get("/todos", async(req, res) => {
-  try {
-    const allTodos = await pool.query("SELECT * FROM todo");
-
-    res.json(allTodos.rows);
-  } catch(err) {
-    console.log(err.message);
-  }
-
-})
+  Todo.findAll()
+  .then((todos) => {
+    return res.json(todos);
+  }).catch((err) => {
+    console.log(err);
+    return res.json(err);
+  });
+});
 
 // Get a todo
 app.get("/todos/:id", async(req, res) => {
-  const { id } = req.params;
+  Todo.findByPk(req.params.id)
+  .then((resolve, reject) => {
+    if(todo)
+      return res.json(resolve);
 
-  try {
-    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [ id ]);
-
-    res.json(todo.rows);
-  } catch(err) {
-    console.log(err.message);
-  }
-})
+    return res.send("Todo doesn't exist.");
+  }).catch((err) => {
+    return res.send(err);
+  });
+});
 
 //Create a todo
 app.post("/todos", async(req, res) => {
-  try {
-    const { description } = req.body;
-    const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", [ description ]);
+  Todo.create({ title: req.body.title })
+  .then((resolve, reject) => {
+    if(resolve)
+      return res.json(resolve);
 
-    res.json(newTodo);
-  } catch(err) {
-    console.error(err.message);
-  }
+    return res.json(reject);
+  }).catch((err) => {
+    return res.send(err);
+  });
 });
 
 // Update a todo
 app.put("/todos/:id", async(req, res) => {
-  const { id } = req.params;
-  const { description } = req.body;
+  Todo.update({ title: req.body.title}, {
+    where: {
+      todoId: req.params.id
+    }
+  }).then((resolve, reject) => {
+    if(resolve)
+      return res.json(resolve);
 
-  try {
-    const updateTodo = pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [ description, id]);
-
-    res.json("Todo was updated");
-  } catch(err) {
-    console.log(err.message);
-  }
-})
+    return res.send(reject);
+  }).catch((err) => {
+    return res.send(err);
+  });
+});
 
 // Delete a todo
 app.delete("/todos/:id", async(req, res) => {
-  try {
-    const { id } = req.params;
-    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [ id ]);
+  Todo.destroy({
+    where:{
+      todoId: req.params.id
+    }
+  }).then((resolve, reject) => {
+    if(resolve)
+      return res.json(resolve);
 
-    res.send("Todo was successfully deleted..");
-  } catch(err) {
-    console.log(err.message);
-  }
-})
+    return res.send(reject);
+  }).catch((err) => {
+    return res.send(err);
+  });
+});
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log("Server has started...");
